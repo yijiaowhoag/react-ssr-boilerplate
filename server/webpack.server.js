3de1810374d2f60c8a3d19ef82fc31a071db5cb0
Yijiao Wang <ttwyj250@gmail.com>
2022-07-02 01:18:42 -0700

    Ignore bundling  for backend

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
   target: 'node',
   entry: './src/index.js',
+  externals: [webpackNodeExternals()],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
